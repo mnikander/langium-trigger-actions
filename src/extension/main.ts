9e8974a4ee49489cefe193b103285333022ba72a
Marco Nikander <m4n1k.developer@gmail.com>
2025-01-15 16:49:07 +0100

    create an simple WebView panel in VS code as described in the official VS code tutorial for Web Views

--- src/extension/main.ts
+++ src/extension/main.ts
@@ -9,6 +9,29 @@ let client: LanguageClient;
 export function activate(context: vscode.ExtensionContext): void {
     client = startLanguageClient(context);
     vscode.window.showInformationMessage("Welcome to VS code with your Langium extension!");
+
+    const panel = vscode.window.createWebviewPanel(
+        'triggerAction', // Identifies the type of the webview. Used internally
+        'Trigger Actions', // Title of the panel displayed to the user
+        vscode.ViewColumn.One,
+        {}
+    );
+
+    panel.webview.html = getWebviewContent();
+}
+
+function getWebviewContent() {
+    return `<!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Cat Coding</title>
+        </head>
+        <body>
+            <img src="https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif" width="300" />
+        </body>
+        </html>`;
 }
 
 // This function is called when the extension is deactivated.
